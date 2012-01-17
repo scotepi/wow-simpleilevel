fae5bfdaf03f09d5f7b34e880f4995abae71b623
Scotepi <1680867+scotepi@users.noreply.github.com>
2012-01-17 19:19:53 +0000

    Fixed commas

--- Locals/ruRU.lua
+++ Locals/ruRU.lua
@@ -2,5 +2,5 @@ local L = LibStub("AceLocale-3.0"):NewLocale("SimpleILevel", "ruRU");
 
 if not L then return end
 
---@localization(locale="ruRU", format="lua_additive_table" handle-unlocalized="comment" handle-subnamespaces="subtable")@
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="comment", handle-subnamespaces="subtable")@
 
