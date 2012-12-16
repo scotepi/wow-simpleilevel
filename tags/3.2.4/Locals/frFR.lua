e7c16f5f46f1121be011a4f93cf74127cf6a29e7
scotepi <scotepi@47da22de-557a-4c3d-957e-29a15e65ac5f>
2012-12-16 21:03:35 +0000

    Tagging as 3.2.4

---
+++ tags/3.2.4/Locals/frFR.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SimpleILevel", "frFR");
+
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="comment", handle-subnamespaces="subtable")@
+
