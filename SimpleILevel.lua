9aeb047be01019b4a0b6f8b72c531a259e1bc3b9
Scotepi <1680867+scotepi@users.noreply.github.com>
2014-05-27 02:13:36 +0000

    Updated item upgrades for 5.4.8

--- SimpleILevel.lua
+++ SimpleILevel.lua
@@ -618,6 +618,7 @@ end
 
 -- Thanks to Ro of Hyjal-US http://us.battle.net/wow/en/forum/topic/7199032730#9
 function SIL:GetActualItemLevel(link)
+    -- Updated for 5.4.8
     local levelAdjust={ -- 11th item:id field and level adjustment
         ["0"]=0,["1"]=8,["373"]=4,["374"]=8,["375"]=4,["376"]=4,
         ["377"]=4,["379"]=4,["380"]=4,["445"]=0,["446"]=4,["447"]=8,
@@ -625,7 +626,8 @@ function SIL:GetActualItemLevel(link)
         ["457"]=8,["458"]=0,["459"]=4,["460"]=8,["461"]=12,["462"]=16,
         ["465"]=0,["466"]=4,["467"]=8,["469"]=4,["470"]=8,["471"]=12,
         ["472"]=16,["491"]=0,["492"]=4,["493"]=8,["494"]=4,["495"]=8,
-        ["496"]=8,["497"]=12,["498"]=16,
+        ["496"]=8,["497"]=12,["498"]=16,["504"]=12,["505"]=16,["506"]=20,
+        ["507"]=24,
     }
     local baseLevel = select(4,GetItemInfo(link))
     local upgrade = link:match(":(%d+)\124h%[")
