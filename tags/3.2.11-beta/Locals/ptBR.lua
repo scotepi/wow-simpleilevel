54b51e4244dd4f400c2356c2c336cde620423f59
scotepi <scotepi@47da22de-557a-4c3d-957e-29a15e65ac5f>
2014-05-27 02:14:48 +0000

    Tagging as 3.2.11-beta

---
+++ tags/3.2.11-beta/Locals/ptBR.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SimpleILevel", "ptBR");
+
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="english", handle-subnamespaces="subtable")@
+
